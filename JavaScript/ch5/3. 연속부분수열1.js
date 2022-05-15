c0f7c950e85c5e30f6b487fa42d5fe593eceb683
JunJongHun <90402926+JunJongHun@users.noreply.github.com>
2022-05-15 20:50:05 +0900

    ch5

--- JavaScript/ch5/3. 연속부분수열1.js
+++ JavaScript/ch5/3. 연속부분수열1.js
@@ -1,4 +1,17 @@
-function solution(m, arr) {}
+function solution(m, arr) {
+  let answer = 0;
+  for (let index = 0; index < arr.length; index++) {
+    let sum = 0;
+    for (let j = index; j < arr.length; j++) {
+      sum += arr[j];
+      if (sum === m) {
+        answer++;
+        break;
+      }
+    }
+  }
+  return answer;
+}
 
 let a = [1, 2, 1, 3, 1, 1, 1, 2];
 console.log(solution(6, a));
