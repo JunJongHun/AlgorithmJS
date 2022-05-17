0337d959c56b91c015f5f8e4fa83ee26da52eb5d
JongHun <minn4072@gmail.com>
2022-05-18 01:20:47 +0900

    ch5

--- JavaScript/ch5/4. 연속부분수열2.js
+++ JavaScript/ch5/4. 연속부분수열2.js
@@ -1,4 +1,24 @@
-function solution(m, arr) {}
+function solution(m, arr) {
+  let pl = 0;
+  let pr = 0;
+  let sum = 0;
+  let count = 0;
+  while (pl < arr.length) {
+    sum += arr[pr];
+    if (sum <= m) {
+      count++;
+      pr++;
+    } else {
+      sum -= arr[pl];
+      sum -= arr[pr];
+      count++;
+      pl++;
+    }
+    console.log(sum);
+  }
+
+  return count;
+}
 
 let a = [1, 3, 1, 2, 3];
 console.log(solution(5, a));
--- JavaScript/ch5/6. 학급 회장(해쉬).js
+++ JavaScript/ch5/6. 학급 회장(해쉬).js
@@ -1,4 +1,24 @@
-function solution(s) {}
+function solution(s) {
+  let a = new Map();
+  let answer;
+  let max = 0;
+  for (let x of s) {
+    if (a.has(x)) {
+      a.set(x, a.get(x) + 1);
+    } else {
+      a.set(x, 0);
+    }
+  }
+
+  for (const [key, value] of a) {
+    if (max < value) {
+      max = value;
+      answer = key;
+    }
+  }
+
+  return answer;
+}
 
 let str = "BACBACCACCBDEDE";
 console.log(solution(str));
