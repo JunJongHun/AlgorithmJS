a32453b052024698859b6360e3f462692d156763
JunJongHun <90402926+JunJongHun@users.noreply.github.com>
2022-05-17 16:26:18 +0900

    ch5

--- JavaScript/ch5/5. 최대 매출.js
+++ JavaScript/ch5/5. 최대 매출.js
@@ -1,4 +1,28 @@
-function solution(k, arr) {}
+function solution(k, arr) {
+  let answer = 0;
+  let sum = 0;
+  let sum2 = 0;
+  let count = 0;
+  let p1 = 0,
+    p2 = 0;
+  for (let i = 0; i < arr.length - k; i++) {
+    if (count < k) {
+      sum += arr[i];
+      count++;
+    } else {
+      p2 = i;
+      sum2 = sum - arr[p1] + arr[p2];
+      p1++;
+      p2++;
+      if (sum < sum2) {
+        sum = sum2;
+      }
+    }
+  }
+  answer = sum;
+
+  return answer;
+}
 
 let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
 console.log(solution(3, a));
