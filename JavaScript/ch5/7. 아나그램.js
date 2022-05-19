5d63c8fb4362196ac73b42be0f82d509b88a74c1
JongHun <minn4072@gmail.com>
2022-05-20 03:13:19 +0900

    ch5 f

--- JavaScript/ch5/7. 아나그램.js
+++ JavaScript/ch5/7. 아나그램.js
@@ -1,5 +1,37 @@
-function solution(str1, str2) {}
+function solution(str1, str2) {
+  let answer = "Yes";
+
+  let a = new Map();
+  let b = new Map();
+
+  for (const i of str1) {
+    if (a.has(i)) a.set(i, a.get(i) + 1);
+    else a.set(i, 1);
+  }
+
+  for (const i of str2) {
+    if (b.has(i)) b.set(i, b.get(i) + 1);
+    else b.set(i, 1);
+  }
+
+  for (const [key, value] of a) {
+    if (!(value === b.get(key))) {
+      answer = "NO";
+      break;
+    }
+  }
+
+  console.log(a);
+  console.log(b);
+
+  return answer;
+}
 
 let a = "AbaAeCe";
 let b = "baeeACA";
+
+let c = "abaCC";
+let d = "Caaab";
+
 console.log(solution(a, b));
+console.log(solution(c, d));
--- JavaScript/ch5/8. 모든 아나그램.js
+++ JavaScript/ch5/8. 모든 아나그램.js
@@ -1,5 +1,66 @@
 function compareMaps(map1, map2) {}
-function solution(s, t) {}
+function solution(s, t) {
+  let answer = 0;
+  let a = new Map();
+  let b = new Map();
+  let lp = 0;
+  let rp = 0;
+  let a_sum = 0;
+  let b_sum = 0;
+  for (const [i, v] of t) {
+    if (b.has(i)) {
+      b.set(i, b.get(i) + 1);
+    } else b.set(i, 1);
+  }
+
+  for (const [i, v] of b) {
+    b_sum += v;
+  }
+  //   console.log(b_sum);
+
+  for (let rp = 0; rp < s.length; rp++) {
+    if (a.has(s[rp])) {
+      a.set(s[rp], a.get(s[rp]) + 1);
+    } else a.set(s[rp], 1);
+    // console.log(a);
+    if (a.size === b.size) {
+      let ok = true;
+      for (const [key, value] of b) {
+        if (!(value === a.get(key))) {
+          ok = false;
+          break;
+        }
+      }
+      //   console.log(ok);
+      if (ok) {
+        answer++;
+      }
+
+      if (a.get(s[lp]) !== 1) {
+        a.set(s[lp], a.get(s[lp]) - 1);
+      } else {
+        a.delete(s[lp]);
+      }
+      lp++;
+    } else {
+      for (const [k, v] of a) {
+        a_sum += v;
+      }
+      //   console.log(a_sum);
+      if (a_sum === b_sum) {
+        if (a.get(s[lp]) !== 1) {
+          a.set(s[lp], a.get(s[lp]) - 1);
+        } else {
+          a.delete(s[lp]);
+        }
+        lp++;
+      }
+      a_sum = 0;
+    }
+  }
+
+  return answer;
+}
 
 let a = "bacaAacba";
 let b = "abc";
