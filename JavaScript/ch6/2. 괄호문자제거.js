a52e319d009a947f5e9e451e3149baf7fe9ae9ec
JongHun <minn4072@gmail.com>
2022-05-23 00:24:33 +0900

    ch6

--- JavaScript/ch6/2. 괄호문자제거.js
+++ JavaScript/ch6/2. 괄호문자제거.js
@@ -1,4 +1,20 @@
-function solution(s) {}
+function solution(s) {
+  let answer;
+
+  let stack = [];
+
+  for (const i of s) {
+    if ("(" === i) {
+      stack.push(i);
+    } else if (")" === i) {
+      while (stack.pop() !== "(") {}
+    } else {
+      stack.push(i);
+    }
+  }
+  answer = stack.join("");
+  return answer;
+}
 
 let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
 console.log(solution(str));
