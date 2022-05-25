660a79569b4db95e13bb8991a1c81cab3eb99035
JunJongHun <90402926+JunJongHun@users.noreply.github.com>
2022-05-25 19:59:46 +0900

    ch6

--- JavaScript/ch6/6. 공주구하기.js
+++ JavaScript/ch6/6. 공주구하기.js
@@ -1,3 +1,18 @@
-function solution(n, k) {}
+function solution(n, k) {
+  let answer = 0;
+  let queue = [];
+  for (let i = 0; i < n; i++) {
+    queue.push(i + 1);
+  }
 
-console.log(solution(8, 3));
+  while (queue.length !== 1) {
+    for (let i = 0; i < k - 1; i++) {
+      queue.push(queue.shift());
+    }
+    queue.shift();
+  }
+  answer = queue.shift();
+  return answer;
+}
+
+console.log(solution(10, 4));
