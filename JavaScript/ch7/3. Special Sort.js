f7a9110575416c8a601367079ba53cdc87aab827
JongHun <minn4072@gmail.com>
2022-05-28 20:44:58 +0900

    ch7

--- JavaScript/ch7/3. Special Sort.js
+++ JavaScript/ch7/3. Special Sort.js
@@ -1,5 +1,15 @@
 function solution(arr) {
-  let answer = arr;
+  let answer;
+
+  let negativeNumber = [];
+  let postiveNumber = [];
+
+  for (const i of arr) {
+    if (i < 0) negativeNumber.push(i);
+    else postiveNumber.push(i);
+  }
+
+  answer = negativeNumber.concat(postiveNumber);
 
   return answer;
 }
@@ -7,10 +17,11 @@ function solution(arr) {
 let arr1 = [1, 2, 3, -3, -2, 5, 6, -6];
 console.log(solution(arr1));
 
-function solution(arr) {
-  let answer = [];
+function solution2(arr) {
+  let answer = arr;
+
   return answer;
 }
 
 let arr2 = [1, 2, 3, -3, -2, 5, 6, -6];
-console.log(solution(arr2));
+console.log(solution2(arr2));
