63d8ad65e7ace9a3df1ce695b8294202ecd9ef67
JongHun <minn4072@gmail.com>
2022-05-30 01:07:25 +0900

    ch7

--- JavaScript/ch7/6. 장난꾸러기 현수.js
+++ JavaScript/ch7/6. 장난꾸러기 현수.js
@@ -1,22 +1,29 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>출력결과</title>
-    </head>
-    <body>
-        <script>
-            function solution(arr){
-                let answer=[];
-                let sortArr=arr.slice();
-                sortArr.sort((a, b)=>a-b);
-                for(let i=0; i<arr.length; i++){
-                    if(arr[i]!==sortArr[i]) answer.push(i+1);
-                }
-                return answer;
-            }
+function solution(arr) {
+  let answer = [];
 
-            let arr=[120, 125, 152, 130, 135, 135, 143, 127, 160];
-            console.log(solution(arr));
-        </script>
-    </body>
-</html>
+  let hunsu;
+  let zzakgung;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] >= arr[i + 1]) {
+      hunsu = i;
+      answer.push(++hunsu);
+      break;
+    }
+  }
+  for (let i = arr.length - 1; i > 0; i--) {
+    if (arr[i] <= arr[i - 1]) {
+      zzakgung = i;
+      answer.push(++zzakgung);
+      break;
+    }
+  }
+
+  return answer;
+}
+
+let arr1 = [120, 125, 152, 130, 135, 135, 143, 127, 160];
+console.log(solution(arr1));
+
+let arr = [120, 130, 150, 150, 130, 150];
+console.log(solution(arr));
