56ff35823d7fd6dcc87fae5f57c367a247aa6b95
JongHun <minn4072@gmail.com>
2022-06-05 01:23:40 +0900

    ch10

--- JavaScript/ch10/2. 돌다리 건너기.js
+++ JavaScript/ch10/2. 돌다리 건너기.js
@@ -1,3 +1,19 @@
-function solution(n) {}
+function solution(n) {
+  let answer;
+
+  let dp = Array.from({ length: 45 }, (v, i) => {
+    return 0;
+  });
+
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n + 1; i++) {
+    dp[i] = dp[i - 2] + dp[i - 1];
+  }
+  answer = dp[n + 1];
+
+  return answer;
+}
 
 console.log(solution(7));
--- JavaScript/ch10/3. 최대부분증가수열(LIS).js
+++ JavaScript/ch10/3. 최대부분증가수열(LIS).js
@@ -1,4 +1,24 @@
-function solution(arr) {}
+function solution(arr) {
+  let answer;
+
+  let dp = Array.from({ length: 1000 }, (v, i) => {
+    return 0;
+  });
+
+  dp[0] = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] < arr[i]) {
+        dp[i] = Math.max(dp[j] + 1, dp[i]);
+      }
+    }
+  }
+
+  answer = Math.max(...dp);
+
+  return answer;
+}
 
 let arr = [5, 3, 7, 8, 6, 2, 9, 4];
 console.log(solution(arr));
