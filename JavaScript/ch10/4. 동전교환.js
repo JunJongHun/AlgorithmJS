df3bd5a976ed076f19583a6a3c7b4ba1b761989b
JongHun <minn4072@gmail.com>
2022-06-05 23:56:43 +0900

    ch10

--- JavaScript/ch10/4. 동전교환.js
+++ JavaScript/ch10/4. 동전교환.js
@@ -1,4 +1,21 @@
-function solution(m, coin) {}
+function solution(m, coin) {
+  let answer = 0;
+  let i = coin.length - 1;
 
-let arr = [1, 2, 5];
-console.log(solution(15, arr));
+  coin.sort((a, b) => a - b);
+
+  while (m > 0) {
+    let quotient = parseInt(m / coin[i]);
+    if (quotient > 0) {
+      answer += quotient;
+      console.log(`${m}을 ${coin[i]}로 나누면 누적 ${answer}`);
+      m = m % coin[i];
+    }
+    i--;
+  }
+
+  return answer;
+}
+
+let arr = [1, 2, 5, 3, 8];
+console.log(solution(23, arr));
