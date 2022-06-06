860620cb18e24b098646557f4299c774562dd923
JongHun <minn4072@gmail.com>
2022-06-07 01:20:33 +0900

    ch10

--- JavaScript/ch10/5. 최대점수 구하기.js
+++ JavaScript/ch10/5. 최대점수 구하기.js
@@ -1,4 +1,21 @@
-function solution(m, arr) {}
+function solution(m, arr) {
+  let answer;
+  let dp = Array.from({ length: 300 }, (v, i) => 0);
+  for (let i = 0; i < arr.length; i++) {
+    let score = arr[i][0];
+    let time = arr[i][1];
+    dp[time] = score;
+  }
+  for (let i = 0; i <= 300; i++) {
+    for (let j = 0; j <= i; j++) {
+      if (dp[i - j] === dp[j]) continue;
+      dp[i] = Math.max(dp[i], dp[i - j] + dp[j]);
+    }
+  }
+  console.log(dp);
+  answer = dp[m];
+  return answer;
+}
 
 let arr = [
   [10, 5],
@@ -7,4 +24,5 @@ let arr = [
   [6, 3],
   [7, 4],
 ];
+
 console.log(solution(20, arr));
