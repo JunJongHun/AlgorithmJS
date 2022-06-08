19d7e43e8d1929b8628845d5893e74181f4b7d90
JongHun <minn4072@gmail.com>
2022-06-09 00:25:44 +0900

    ch10

--- JavaScript/ch10/4. 동전교환.js
+++ JavaScript/ch10/4. 동전교환.js
@@ -19,3 +19,21 @@ function solution(m, coin) {
 
 let arr = [1, 2, 5, 3, 8];
 console.log(solution(23, arr));
+
+function solution1(m, coin) {
+  let answer = 0;
+  let dp = Array.from({ length: m + 1 }, (v, i) => 1000);
+  dp[0] = 0;
+
+  for (const coco of coin) {
+    for (let i = coco; i <= m; i++) {
+      dp[i] = Math.min(dp[i], dp[i - coco] + 1);
+    }
+  }
+
+  answer = dp[m];
+  return answer;
+}
+
+let arr1 = [1, 2, 5, 3, 8];
+console.log(solution1(17, arr));
