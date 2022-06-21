d29ab0614e9a4afb8ddb419e9e0f2e5c479ff73a
JongHun <minn4072@gmail.com>
2022-06-21 23:22:30 +0900

    ch8

--- JavaScript/ch8/4. 부분집합 구하기.js
+++ JavaScript/ch8/4. 부분집합 구하기.js
@@ -1,21 +1,20 @@
-function DFS(L) {
-  if (L === n + 1) {
-    let tmp = "";
-    for (let i = 1; i <= n; i++) {
-      if (ch[i] === 1) tmp += i + " ";
-    }
-    if (tmp.length > 0) answer.push(tmp.trim());
-  } else {
-    ch[L] = 1;
-    DFS(L + 1);
-    ch[L] = 0;
-    DFS(L + 1);
-  }
-}
-
 function solution(n) {
   let answer = [];
   let ch = Array.from({ length: n + 1 }, () => 0);
+  function DFS(L) {
+    if (L === n + 1) {
+      let tmp = [];
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 1) tmp.push(i);
+      }
+      if (tmp.length > 0) answer.push(tmp);
+    } else {
+      ch[L] = 1;
+      DFS(L + 1);
+      ch[L] = 0;
+      DFS(L + 1);
+    }
+  }
   DFS(1);
   return answer;
 }
