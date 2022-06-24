ed83656da51284b053edaf95d4943a26aafe5f4c
JongHun <minn4072@gmail.com>
2022-06-24 11:57:36 +0900

    ch8

--- JavaScript/ch8/6. 바둑이 승차.js
+++ JavaScript/ch8/6. 바둑이 승차.js
@@ -1,4 +1,28 @@
-function solution(c, arr) {}
+function solution(c, arr) {
+  let answer;
+  let max = 0;
+  let ch = Array.from({ length: arr.length }, (v, i) => 0);
+
+  function DFS(L) {
+    if (L === arr.length) {
+      let sum = 0;
+      for (let i = 0; i < arr.length; i++) {
+        if (ch[i] === 1) sum += arr[i];
+      }
+      if (sum < c && sum > max) {
+        max = sum;
+        answer = max;
+      }
+    } else {
+      ch[L] = 1;
+      DFS(L + 1);
+      ch[L] = 0;
+      DFS(L + 1);
+    }
+  }
+  DFS(0);
+  return answer;
+}
 
 let arr = [81, 58, 42, 33, 61];
 console.log(solution(259, arr));
--- JavaScript/ch8/7. 최대점수 구하기.js
+++ JavaScript/ch8/7. 최대점수 구하기.js
@@ -1,4 +1,35 @@
-function solution(m, ps, pt) {}
+function solution(m, ps, pt) {
+  let answer;
+  let max = 0;
+
+  let ch = Array.from({ length: ps.length }, (v, i) => 0);
+
+  function DFS(L) {
+    if (L === ps.length) {
+      let time_sum = 0;
+      let score_sum = 0;
+      for (let i = 0; i < pt.length; i++) {
+        if (ch[i] === 1) time_sum += pt[i];
+      }
+      if (time_sum <= m) {
+        for (let i = 0; i < ps.length; i++) {
+          if (ch[i] === 1) score_sum += ps[i];
+        }
+        if (score_sum > max) {
+          max = score_sum;
+          answer = max;
+        }
+      }
+    } else {
+      ch[L] = 1;
+      DFS(L + 1);
+      ch[L] = 0;
+      DFS(L + 1);
+    }
+  }
+  DFS(0);
+  return answer;
+}
 
 let ps = [10, 25, 15, 6, 7];
 let pt = [5, 12, 8, 3, 4];
