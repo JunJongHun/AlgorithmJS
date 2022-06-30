97826da0e0d44ac0d95b5763c55bf150aa895e04
JongHun <minn4072@gmail.com>
2022-06-30 20:56:38 +0900

    ch8

--- JavaScript/ch8/8. 중복순열.js
+++ JavaScript/ch8/8. 중복순열.js
@@ -1,8 +1,21 @@
 function solution(n, m) {
+  let answer = [];
 
+  let ch = Array.from({ length: m }, () => 0);
 
-
-    
+  function DFS(L) {
+    if (L === m) {
+      console.log(ch[0], ch[1]);
+      return;
+    } else {
+      for (let i = 1; i <= n; i++) {
+        ch[L] = i;
+        DFS(L + 1);
+      }
+    }
+  }
+  DFS(0);
+  return answer;
 }
 
 console.log(solution(3, 2));
