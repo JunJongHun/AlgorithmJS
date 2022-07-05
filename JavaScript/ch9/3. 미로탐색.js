5777eaae23403b67b912294aeb27214ed79912d6
JunJongHun <90402926+JunJongHun@users.noreply.github.com>
2022-07-05 23:54:52 +0900

    ch9

--- JavaScript/ch9/3. 미로탐색.js
+++ JavaScript/ch9/3. 미로탐색.js
@@ -1,4 +1,29 @@
-function solution(board) {}
+function solution(board) {
+  let answer = 0;
+  let dy = [-1, 1, 0, 0];
+  let dx = [0, 0, -1, 1];
+
+  function DFS(x, y) {
+    if (x === 6 && y === 6) answer++;
+    else {
+      for (let i = 0; i < 4; i++) {
+        let ny = y + dy[i];
+        let nx = x + dx[i];
+        if (0 <= ny && ny <= 6 && 0 <= nx && nx < 6) {
+          if (board[nx][ny] === 0) {
+            board[nx][ny] = 1;
+
+            DFS(nx, ny);
+            board[nx][ny] = 0;
+          }
+        }
+      }
+    }
+  }
+  board[0][0] = 1;
+  DFS(0, 0);
+  return answer;
+}
 
 let arr = [
   [0, 0, 0, 0, 0, 0, 0],
