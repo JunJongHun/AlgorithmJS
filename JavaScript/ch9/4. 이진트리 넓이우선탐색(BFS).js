30960970351830925be3b85b08e54d1b631ac6fd
JongHun <minn4072@gmail.com>
2022-07-07 23:06:47 +0900

    ch9

--- JavaScript/ch9/4. 이진트리 넓이우선탐색(BFS).js
+++ JavaScript/ch9/4. 이진트리 넓이우선탐색(BFS).js
@@ -1,3 +1,20 @@
-function solution() {}
+function solution() {
+  let answer = [];
+
+  let queue = [1];
+
+  while (queue.length) {
+    let data = queue.shift();
+
+    answer.push(data);
+    if (data !== 7) {
+      for (const i of [data * 2, data * 2 + 1]) {
+        queue.push(i);
+      }
+    } else break;
+  }
+  console.log(answer);
+  return;
+}
 
 console.log(solution());
